d0daf0dd45fa89c8587399dc06eb2a82329cdbd0
NedcloarBR <nedcloar1@hotmail.com>
2024-10-06 15:10:32 -0300

    feat: configure `bitnami/postgresql:17` in Docker

---
+++ prisma/migrations/20241006180636_create_users_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
